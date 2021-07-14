069e574f697f50cfac83a3cea578edabba99ff3d
mirusona <mirusona3@gmail.com>
2021-07-14 17:33:28 +0900

    molepang v1.0

---
+++ Assets/Script/TestPlayer.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestPlayer : MonoBehaviour {
+
+    public float limit_height = 1.3f;
+    public float init_height = 2.0f;
+    public float speed = 0.02f;
+
+    public Transform test_cube;
+    public Mole test_mole;
+
+    //초기화
+    void Start()
+    {
+
+    }
+
+    //업데이트
+    void FixedUpdate()
+    {
+        Vector3 pos = test_cube.position;
+        if (Input.GetKey(KeyCode.A))
+        {
+            if (pos.y > limit_height)
+                test_cube.Translate(Vector3.down * speed);
+            else
+                test_cube.position = new Vector3(pos.x, limit_height, pos.z);
+        }
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            test_mole.Init();
+            test_mole.gameObject.SetActive(true);
+        }
+
+        if (!Input.anyKey)
+        {
+            if (pos.y < init_height)
+                test_cube.Translate(Vector3.up * speed * 2f);
+            else
+                test_cube.position = new Vector3(pos.x, init_height, pos.z);
+        }
+    }
+}
---
+++ Assets/Script/Util/Define.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Define {
+
+    //타이머
+    public class Timer
+    {
+
+        public float time = 0;
+        public float limit = 0;
+        public bool end = false;
+
+        public Timer(float _time, float _limit)
+        {
+            time = _time;
+            limit = _limit;
+        }
+
+        //시간 자동 체크
+        public bool AutoTimer()
+        {
+            end = false;
+            if (time < limit)
+                time += Time.deltaTime;
+            else
+            {
+                time = 0;
+                end = true;
+            }
+            return end;
+        }
+
+        //시간만++
+        public void AddTimer()
+        {
+            if (time < limit)
+                time += Time.deltaTime;
+        }
+
+        //시간초기화
+        public void InitTimer()
+        {
+            time = 0;
+        }
+
+        //시간 됬는지 체크
+        public bool CheckTimer()
+        {
+            end = false;
+            if (time >= limit)
+            {
+                end = true;
+            }
+            return end;
+        }
+    }
+}
